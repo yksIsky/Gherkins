8235d70cc1aadcd73bc3c74e48655febac44f148
yks <yks@yks.com>
2025-04-08 12:21:37 +0200

    test

--- features/steps/test_steps.py
+++ features/steps/test_steps.py
@@ -1,18 +1,18 @@
 from behave import given, when, then
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 @given('I have username "user1" and password "pass123"')
 def step_given_user_pass(context):
+    options = Options()
+    options.add_argument("--headless")  # tryb bez GUI
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    context.driver = webdriver.Chrome(options=options)
     context.username = "user1"
     context.password = "pass123"
-    # Inicjalizacja drivera
-    context.driver = webdriver.Chrome()
     context.wait = WebDriverWait(context.driver, 10)
-
 
 @when("I log in")
 def step_when_login(context):
@@ -21,7 +21,6 @@ def step_when_login(context):
     else:
         context.message = "Login failed"
 
-
 @then('I should see "Welcome user1"')
 def step_then_see_message(context):
     assert context.message == "Welcome user1"
@@ -29,18 +28,9 @@ def step_then_see_message(context):
     wait = context.wait
 
     driver.get("https://demoqa.com/")
-
-    # Poczekaj aż będzie klikalny i kliknij przez JS (ze względu na reklamy)
     element = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='card-body']/h5[1]")))
     driver.execute_script("arguments[0].click();", element)
-
-    # Kliknij na "Text Box"
     wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='item-0']/span"))).click()
 
-    # Poczekaj aż pojawi się etykieta "Full Name"
     label = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='userName-label']")))
-
-    print(label.text)
     assert label.text == "Full Name"
-
-    driver.quit()
